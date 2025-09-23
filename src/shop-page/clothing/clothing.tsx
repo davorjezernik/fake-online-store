03b26ffed5a9a9943a2f66fc8d74194e15c72618
Davor Jezernik <369davor@gmail.com>
2025-09-23 21:27:47 +0200

    issues passing the API info from parent to child

--- src/shop-page/clothing/clothing.tsx
+++ src/shop-page/clothing/clothing.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function Clothing() {
+
+  function clothingList() {
+    const [item, setItem ] = itemState<items[]>([])
+    useEffect(() => {
+      fetchAPI().then((date) => setItem(data));
+    }, []);
+  }
+
   return (
-    <h1>Clothing</h1>
+    <div>
+      {item.map((item) => (
+        <div key={item.id}>
+          <p>{item.title}</p>
+          <p>{item.price}</p>
+          <p>{item.description}</p>
+          <p>{item.category}</p>
+        </div>
+      ))}
+    </div>
   );
 }
 
--- src/shop-page/shop-page.tsx
+++ src/shop-page/shop-page.tsx
@@ -3,16 +3,24 @@ import './shop-page.css';
 import Navbar from '../shared/navbar/Navbar';
 import Footer from '../shared/footer/Footer';
 import { Outlet, Link } from 'react-router-dom';
+import Clothing from './clothing/clothing';
 
-function ShopPage() {
+  interface items {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    category: string;
+  }
 
   async function fetchAPI() {
     const URL = 'https://fakestoreapi.com/products';
-    const itemsData = await fetch(URL);
-    const data = await itemsData.json();
+    const response = await fetch(URL);
+    const data = await response.json();
     return data;
   } 
-  
+
+function ShopPage() {
   return (
     <section className='flex-align-justify'>
       <div className='main-container'>
@@ -21,9 +29,15 @@ function ShopPage() {
         </div>
         <div className='layout-shop'>
           <div className='shop-links'>
-            <Link className='shop-links-style' to='/shop/clothing' onClick={fetchAPI}>Clothing</Link>
-            <Link className='shop-links-style' to='/shop/jewelery'>Jewelery</Link>
-            <Link className='shop-links-style' to='/shop/electronics'>Electronics</Link>
+            <Link className='shop-links-style' to='/shop/clothing' onClick={clothingList()}>
+              Clothing
+            </Link>
+            <Link className='shop-links-style' to='/shop/jewelery'>
+              Jewelery
+            </Link>
+            <Link className='shop-links-style' to='/shop/electronics'>
+              Electronics
+            </Link>
           </div>
           <div className='shop-layout-products'>
             <Outlet /> 
